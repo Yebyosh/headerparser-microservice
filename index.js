aa99651034179e861fdbdd3e226817eda6f1d2d5
Yebyosh <jappalang@gmail.com>
2024-10-02 14:20:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || req.socket.remoteAddress;
+  const lng = req.headers['accept-language'];
+  const sw = req.headers['user-agent'];
+
+  res.json({ipaddress: ip, language: lng, software: sw});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
